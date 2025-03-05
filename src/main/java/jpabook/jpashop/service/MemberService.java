0fb50811f82e7a34eeba9cc0fa00ec2f7b39efe6
dytpq <suhokym@gmail.com>
2025-03-05 20:22:06 +0900

    v2 memberrequest

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -44,6 +44,7 @@ public class MemberService {
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
+
     @Transactional(readOnly = true)
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
